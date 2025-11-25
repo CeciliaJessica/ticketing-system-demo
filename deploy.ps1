0480d4fd925fda2da4657a59210b3e958a8b3103
CeciliaJessica <ceciliajhg@hotmail.com>
2025-11-25 21:30:02 +0100

    fix dashboard

--- deploy.ps1
+++ deploy.ps1
@@ -1,13 +1,13 @@
 # Build images
-docker build -t cecilia08/api-gateway:v1 ./api-gateway
-docker build -t cecilia08/ticket-service:v1 ./ticket-service
-docker build -t cecilia08/waiting-room-service:v1 ./waiting-room-service
-docker build -t cecilia08/dashboard-service:v1 ./dashboard-service
-docker build -t cecilia08/website-dashboard:v1 ./website-dashboard
+docker build -t cecilia08/api-gateway:v2 ./api-gateway
+docker build -t cecilia08/ticket-service:v2 ./ticket-service
+docker build -t cecilia08/waiting-room-service:v2 ./waiting-room-service
+docker build -t cecilia08/dashboard-service:v2 ./dashboard-service
+docker build -t cecilia08/website-dashboard:v2 ./website-dashboard
 
 # Push images to Docker Hub
-docker push cecilia08/api-gateway:v1
-docker push cecilia08/ticket-service:v1
-docker push cecilia08/waiting-room-service:v1
-docker push cecilia08/dashboard-service:v1
-docker push cecilia08/website-dashboard:v1
+docker push cecilia08/api-gateway:v2
+docker push cecilia08/ticket-service:v2
+docker push cecilia08/waiting-room-service:v2
+docker push cecilia08/dashboard-service:v2
+docker push cecilia08/website-dashboard:v2
