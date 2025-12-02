9d1a1784d86be01c2b96e643d946c954c6ee794c
Diego <53939730+DiegoIEC@users.noreply.github.com>
2025-12-02 23:53:10 +0100

    v5

--- deploy.ps1
+++ deploy.ps1
@@ -1,13 +1,13 @@
 # Build images
-docker build -t cecilia08/api-gateway:v2 ./api-gateway
-docker build -t cecilia08/ticket-service:v4 ./ticket-service
-docker build -t cecilia08/waiting-room-service:v2 ./waiting-room-service
-docker build -t cecilia08/dashboard-service:v2 ./dashboard-service
-docker build -t cecilia08/website-dashboard:v2 ./website-dashboard
+docker build -t cecilia08/api-gateway:v5 ./api-gateway
+docker build -t cecilia08/ticket-service:v5 ./ticket-service
+docker build -t cecilia08/waiting-room-service:v5 ./waiting-room-service
+docker build -t cecilia08/dashboard-service:v5 ./dashboard-service
+docker build -t cecilia08/website-dashboard:v5 ./website-dashboard
 
 # Push images to Docker Hub
-docker push cecilia08/api-gateway:v2
-docker push cecilia08/ticket-service:v4
-docker push cecilia08/waiting-room-service:v2
-docker push cecilia08/dashboard-service:v2
-docker push cecilia08/website-dashboard:v2
+docker push cecilia08/api-gateway:v5
+docker push cecilia08/ticket-service:v5
+docker push cecilia08/waiting-room-service:v5
+docker push cecilia08/dashboard-service:v5
+docker push cecilia08/website-dashboard:v5
