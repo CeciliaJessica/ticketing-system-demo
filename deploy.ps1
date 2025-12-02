9c45ae4b5dcd344caa6ab09553ddb89be67d5f6c
Diego <53939730+DiegoIEC@users.noreply.github.com>
2025-12-02 22:12:43 +0100

    cluster db

--- deploy.ps1
+++ deploy.ps1
@@ -1,13 +1,13 @@
 # Build images
 docker build -t cecilia08/api-gateway:v2 ./api-gateway
-docker build -t cecilia08/ticket-service:v2 ./ticket-service
+docker build -t cecilia08/ticket-service:v4 ./ticket-service
 docker build -t cecilia08/waiting-room-service:v2 ./waiting-room-service
 docker build -t cecilia08/dashboard-service:v2 ./dashboard-service
 docker build -t cecilia08/website-dashboard:v2 ./website-dashboard
 
 # Push images to Docker Hub
 docker push cecilia08/api-gateway:v2
-docker push cecilia08/ticket-service:v2
+docker push cecilia08/ticket-service:v4
 docker push cecilia08/waiting-room-service:v2
 docker push cecilia08/dashboard-service:v2
 docker push cecilia08/website-dashboard:v2
