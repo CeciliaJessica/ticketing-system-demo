6850aa9021e5c7e70cdf3dd0cfa3f1836d9d9289
CeciliaJessica <ceciliajhg@hotmail.com>
2025-12-03 02:40:21 +0100

    fix

--- deploy.ps1
+++ deploy.ps1
@@ -1,13 +1,13 @@
 # Build images
 docker build -t cecilia08/api-gateway:v5 ./api-gateway
-docker build -t cecilia08/ticket-service:v6 ./ticket-service
+docker build -t cecilia08/ticket-service:v7 ./ticket-service
 docker build -t cecilia08/waiting-room-service:v5 ./waiting-room-service
 docker build -t cecilia08/dashboard-service:v5 ./dashboard-service
 docker build -t cecilia08/website-dashboard:v5 ./website-dashboard
 
 # Push images to Docker Hub
 docker push cecilia08/api-gateway:v5
-docker push cecilia08/ticket-service:v6
+docker push cecilia08/ticket-service:v7
 docker push cecilia08/waiting-room-service:v5
 docker push cecilia08/dashboard-service:v5
 docker push cecilia08/website-dashboard:v5
